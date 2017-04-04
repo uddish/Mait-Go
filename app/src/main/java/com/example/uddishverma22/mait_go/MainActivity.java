a79d95fe5c0476b629b71694fd0ec5a5c94c51ba
uddish <uddishverma22@gmail.com>
2017-04-04 10:59:59 +0530

    Added schedule activity

--- app/src/main/java/com/example/uddishverma22/mait_go/MainActivity.java
+++ app/src/main/java/com/example/uddishverma22/mait_go/MainActivity.java
@@ -22,7 +22,7 @@ public class MainActivity extends AppCompatActivity
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-        
+
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
