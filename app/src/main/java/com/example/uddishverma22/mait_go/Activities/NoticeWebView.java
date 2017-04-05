6bd9fb103dc2080751811e4ef8f993f990bf77bb
Uddish <uddish@xprep.co>
2017-04-05 20:47:08 +0530

    Noticr webview added

---
+++ app/src/main/java/com/example/uddishverma22/mait_go/Activities/NoticeWebView.java
@@ -0,0 +1,15 @@
+package com.example.uddishverma22.mait_go.Activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.uddishverma22.mait_go.R;
+
+public class NoticeWebView extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_notice_web_view);
+    }
+}
