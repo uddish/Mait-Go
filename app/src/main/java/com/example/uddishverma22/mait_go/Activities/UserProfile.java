b97d03dfee3fc57c64b363725f49c3184fc4aab7
uddish <uddishverma22@gmail.com>
2017-04-07 12:43:36 +0530

    Add custom alert

--- app/src/main/java/com/example/uddishverma22/mait_go/Activities/UserProfile.java
+++ app/src/main/java/com/example/uddishverma22/mait_go/Activities/UserProfile.java
@@ -20,11 +20,11 @@ import rebus.bottomdialog.BottomDialog;
 
 public class UserProfile extends AppCompatActivity {
 
-    TextView name, roll, branch, branchHeading, classHeading, semesterHeading;
+    TextView name, roll, branch, semester, branchHeading, classHeading, semesterHeading;
     ImageView orangeTheme;
     public static int themeColor = 0;
     RelativeLayout relativeLayout;
-    LinearLayout branchSelector;
+    LinearLayout branchSelector, semesterSelector;
     BottomDialog dialog;
     Typeface tf;
 
@@ -43,8 +43,12 @@ public class UserProfile extends AppCompatActivity {
         roll = (TextView) findViewById(R.id.enrollment_tv);
         orangeTheme = (ImageView) findViewById(R.id.orange_theme);
         relativeLayout = (RelativeLayout) findViewById(R.id.diagonal_view);
+
         branch = (TextView) findViewById(R.id.branch_tv);
+        semester = (TextView) findViewById(R.id.semester_tv);
+
         branchSelector = (LinearLayout) findViewById(R.id.branch_selector);
+        semesterSelector = (LinearLayout) findViewById(R.id.semester_selector);
 
         branchHeading = (TextView) findViewById(R.id.branch_heading);
         classHeading = (TextView) findViewById(R.id.class_heading);
@@ -57,39 +61,13 @@ public class UserProfile extends AppCompatActivity {
         branchSelector.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                dialog = new BottomDialog(UserProfile.this);
-                dialog.title("Select your branch...");
-                dialog.canceledOnTouchOutside(true);
-                dialog.cancelable(true);
-                dialog.inflateMenu(R.menu.branch_menu);
-                dialog.setOnItemSelectedListener(new BottomDialog.OnItemSelectedListener() {
-                    @Override
-                    public boolean onItemSelected(int id) {
-                        switch (id) {
-                            case R.id.cse:
-                                branch.setText("CSE");
-                                branch.setTypeface(tf);
-                                return true;
-                            case R.id.it:
-                                branch.setText("IT");
-                                return true;
-                            case R.id.ece:
-                                branch.setText("ECE");
-                                return true;
-                            case R.id.eee:
-                                branch.setText("EEE");
-                                return true;
-                            case R.id.mae:
-                                branch.setText("MAE");
-                                return true;
-                            case 100:
-                                return false;
-                            default:
-                                return false;
-                        }
-                    }
-                });
-                dialog.show();
+                branchSelectorClicked();
+            }
+        });
+        semesterSelector.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                semesterSelectorClicked();
             }
         });
 
@@ -109,6 +87,99 @@ public class UserProfile extends AppCompatActivity {
 
             }
         });
+    }
+
+    private void branchSelectorClicked()    {
+
+        dialog = new BottomDialog(UserProfile.this);
+        dialog.title("Select your branch...");
+        dialog.canceledOnTouchOutside(true);
+        dialog.cancelable(true);
+        dialog.inflateMenu(R.menu.branch_menu);
+        dialog.setOnItemSelectedListener(new BottomDialog.OnItemSelectedListener() {
+            @Override
+            public boolean onItemSelected(int id) {
+                switch (id) {
+                    case R.id.cse:
+                        branch.setText("CSE");
+                        branch.setTypeface(tf);
+                        return true;
+                    case R.id.it:
+                        branch.setText("IT");
+                        branch.setTypeface(tf);
+                        return true;
+                    case R.id.ece:
+                        branch.setText("ECE");
+                        branch.setTypeface(tf);
+                        return true;
+                    case R.id.eee:
+                        branch.setText("EEE");
+                        branch.setTypeface(tf);
+                        return true;
+                    case R.id.mae:
+                        branch.setText("MAE");
+                        branch.setTypeface(tf);
+                        return true;
+                    case 100:
+                        return false;
+                    default:
+                        return false;
+                }
+            }
+        });
+        dialog.show();
+    }
+
+    private void semesterSelectorClicked()  {
+        dialog = new BottomDialog(UserProfile.this);
+        dialog.title("Select your semester...");
+        dialog.canceledOnTouchOutside(true);
+        dialog.cancelable(true);
+        dialog.inflateMenu(R.menu.semester_menu);
+        dialog.setOnItemSelectedListener(new BottomDialog.OnItemSelectedListener() {
+            @Override
+            public boolean onItemSelected(int id) {
+                switch (id) {
+                    case R.id.first:
+                        semester.setText("I");
+                        semester.setTypeface(tf);
+                        return true;
+                    case R.id.second:
+                        semester.setText("II");
+                        semester.setTypeface(tf);
+                        return true;
+                    case R.id.third:
+                        semester.setText("III");
+                        semester.setTypeface(tf);
+                        return true;
+                    case R.id.fourth:
+                        semester.setText("IV");
+                        semester.setTypeface(tf);
+                        return true;
+                    case R.id.fifth:
+                        semester.setText("V");
+                        semester.setTypeface(tf);
+                        return true;
+                    case R.id.sixth:
+                        semester.setText("VI");
+                        semester.setTypeface(tf);
+                        return true;
+                    case R.id.seventh:
+                        semester.setText("VII");
+                        semester.setTypeface(tf);
+                        return true;
+                    case R.id.eighth:
+                        semester.setText("VIII");
+                        semester.setTypeface(tf);
+                        return true;
+                    case 100:
+                        return false;
+                    default:
+                        return false;
+                }
+            }
+        });
+        dialog.show();
     }
 
 }
