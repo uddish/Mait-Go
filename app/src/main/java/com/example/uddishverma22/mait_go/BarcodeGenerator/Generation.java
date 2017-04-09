96e831070a404c6626776984c09cce175c0df8e9
uddish <uddishverma22@gmail.com>
2017-04-09 15:46:57 +0530

    Tested barcode generation

---
+++ app/src/main/java/com/example/uddishverma22/mait_go/BarcodeGenerator/Generation.java
@@ -0,0 +1,104 @@
+package com.example.uddishverma22.mait_go.BarcodeGenerator;
+
+import android.app.Activity;
+import android.graphics.Bitmap;
+import android.media.Image;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+
+import com.example.uddishverma22.mait_go.R;
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.EncodeHintType;
+import com.google.zxing.MultiFormatWriter;
+import com.google.zxing.WriterException;
+import com.google.zxing.common.BitMatrix;
+
+import java.util.EnumMap;
+import java.util.Map;
+import java.util.Objects;
+
+/**
+ * Created by uddishverma22 on 09/04/17.
+ */
+
+public class Generation extends Activity {
+
+    private static final int WHITE = 0xFFFFFFFF;
+    private static final int BLACK = 0xFF000000;
+    ImageView barcodeImg;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        LinearLayout layout= new LinearLayout(this);
+        layout.setLayoutParams(new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT));
+        layout.setOrientation(LinearLayout.VERTICAL);
+
+        setContentView(R.layout.temp);
+
+        barcodeImg = (ImageView) findViewById(R.id.barcode_img);
+
+        String barcodeData = "123456";
+
+        Bitmap bitmap = null;
+//        ImageView iv = new ImageView(this);
+
+        bitmap = encodeAsBitmap(barcodeData, BarcodeFormat.CODE_128, 600, 300);
+        barcodeImg.setImageBitmap(bitmap);
+//        iv.setImageBitmap(bitmap);
+
+//        layout.addView(iv);
+    }
+
+    Bitmap encodeAsBitmap(String contents, BarcodeFormat format, int img_width, int img_height) {
+        String contentsToEncode = contents;
+        if(contentsToEncode == null)    {
+            return null;
+        }
+
+        Map<EncodeHintType, Object> hints = null;
+        String encoding = guessAppropriateEncoding(contentsToEncode);
+        if (encoding != null) {
+            hints = new EnumMap<EncodeHintType, Object>(EncodeHintType.class);
+            hints.put(EncodeHintType.CHARACTER_SET, encoding);
+        }
+        MultiFormatWriter writer = new MultiFormatWriter();
+        BitMatrix result = null;
+        try {
+            result = writer.encode(contentsToEncode, format, img_width, img_height, hints);
+        } catch (IllegalArgumentException iae) {
+            // Unsupported format
+            return null;
+        } catch (WriterException e) {
+            e.printStackTrace();
+        }
+        int width = result.getWidth();
+        int height = result.getHeight();
+        int[] pixels = new int[width * height];
+        for (int y = 0; y < height; y++) {
+            int offset = y * width;
+            for (int x = 0; x < width; x++) {
+                pixels[offset + x] = result.get(x, y) ? BLACK : WHITE;
+            }
+        }
+
+        Bitmap bitmap = Bitmap.createBitmap(width, height,
+                Bitmap.Config.ARGB_8888);
+        bitmap.setPixels(pixels, 0, width, 0, 0, width, height);
+        return bitmap;
+    }
+
+    private static String guessAppropriateEncoding(CharSequence contents) {
+        // Very crude at the moment
+        for (int i = 0; i < contents.length(); i++) {
+            if (contents.charAt(i) > 0xFF) {
+                return "UTF-8";
+            }
+        }
+        return null;
+    }
+}
