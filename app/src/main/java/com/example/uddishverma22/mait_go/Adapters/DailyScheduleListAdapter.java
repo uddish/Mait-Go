256e4efd2f63c972f98b6853285aed020659d6a3
Uddish <uddish@xprep.co>
2017-04-22 13:57:41 +0530

    Recycler view animations added in schedule

--- app/src/main/java/com/example/uddishverma22/mait_go/Adapters/DailyScheduleListAdapter.java
+++ app/src/main/java/com/example/uddishverma22/mait_go/Adapters/DailyScheduleListAdapter.java
@@ -5,7 +5,11 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.view.animation.ScaleAnimation;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.example.uddishverma22.mait_go.Activities.UserProfile;
@@ -13,6 +17,7 @@ import com.example.uddishverma22.mait_go.Models.DailySchedule;
 import com.example.uddishverma22.mait_go.R;
 
 import java.util.List;
+import java.util.Random;
 
 /**
  * Created by uddishverma22 on 04/04/17.
@@ -21,6 +26,7 @@ import java.util.List;
 public class DailyScheduleListAdapter extends RecyclerView.Adapter<DailyScheduleListAdapter.detailsViewHolder> {
 
     public List<DailySchedule> schedule;
+    private int lastPosition = -1;
 
     public static final String TAG = "DailySchedule";
 
@@ -32,6 +38,8 @@ public class DailyScheduleListAdapter extends RecyclerView.Adapter<DailySchedule
 
         private TextView time, subject, room, teacher;
         private ImageView dotColor;
+        private LinearLayout mContainer;
+        private Animation nextAnim;
 
         public detailsViewHolder(View itemView) {
             super(itemView);
@@ -40,6 +48,8 @@ public class DailyScheduleListAdapter extends RecyclerView.Adapter<DailySchedule
             room = (TextView) itemView.findViewById(R.id.room);
             teacher = (TextView) itemView.findViewById(R.id.teacher);
             dotColor = (ImageView) itemView.findViewById(R.id.dot_design);
+            mContainer = (LinearLayout) itemView.findViewById(R.id.container);
+            nextAnim = AnimationUtils.loadAnimation(itemView.getContext(), R.anim.translate_next);
         }
     }
 
@@ -52,6 +62,9 @@ public class DailyScheduleListAdapter extends RecyclerView.Adapter<DailySchedule
 
     @Override
     public void onBindViewHolder(detailsViewHolder holder, int position) {
+
+        setAnimation(holder.itemView, position);
+
         DailySchedule schedulelist = schedule.get(position);
         holder.time.setText(schedulelist.getTime());
         holder.subject.setText(schedulelist.getSubject());
@@ -66,5 +79,15 @@ public class DailyScheduleListAdapter extends RecyclerView.Adapter<DailySchedule
     @Override
     public int getItemCount() {
         return schedule.size();
+    }
+
+    private void setAnimation(View viewToAnimate, int position) {
+        // If the bound view wasn't previously displayed on screen, it's animated
+        if (position > lastPosition) {
+            ScaleAnimation anim = new ScaleAnimation(0.0f, 1.0f, 0.0f, 1.0f, Animation.RELATIVE_TO_SELF, 0.5f, Animation.RELATIVE_TO_SELF, 0.5f);
+            anim.setDuration(new Random().nextInt(501));//to make duration random number between [0,501)
+            viewToAnimate.startAnimation(anim);
+            lastPosition = position;
+        }
     }
 }
