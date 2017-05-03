f044a46d9288678fe660e0bdee97bfdd5742c3da
Uddish <uddish@xprep.co>
2017-05-03 19:40:03 +0530

    Created announcement activity

---
+++ app/src/main/java/com/example/uddishverma22/mait_go/Models/ClassAnnouncementsModel.java
@@ -0,0 +1,47 @@
+package com.example.uddishverma22.mait_go.Models;
+
+import io.realm.RealmObject;
+
+/**
+ * Created by uddishverma22 on 03/05/17.
+ */
+
+public class ClassAnnouncementsModel extends RealmObject {
+
+    public String teacherName;
+    public String msgDate;
+    public String msgTime;
+    public String announcement;
+
+    public String getTeacherName() {
+        return teacherName;
+    }
+
+    public void setTeacherName(String teacherName) {
+        this.teacherName = teacherName;
+    }
+
+    public String getMsgDate() {
+        return msgDate;
+    }
+
+    public void setMsgDate(String msgDate) {
+        this.msgDate = msgDate;
+    }
+
+    public String getMsgTime() {
+        return msgTime;
+    }
+
+    public void setMsgTime(String msgTime) {
+        this.msgTime = msgTime;
+    }
+
+    public String getAnnouncement() {
+        return announcement;
+    }
+
+    public void setAnnouncement(String announcement) {
+        this.announcement = announcement;
+    }
+}
