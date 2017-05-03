723f321a9e0a44e772b90526fb9b7c81e71f36c3
uddish <uddishverma22@gmail.com>
2017-05-03 19:40:03 +0530

    Created announcement activity

---
+++ app/src/main/java/com/example/uddishverma22/mait_go/Activities/Announcements.java
@@ -0,0 +1,15 @@
+package com.example.uddishverma22.mait_go.Activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.uddishverma22.mait_go.R;
+
+public class Announcements extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_announcements);
+    }
+}
