d8a893da7e705d0f8673cc80569399e2bb066da0
Uddish <uddish@xprep.co>
2017-05-05 01:13:23 +0530

    Announcement activity COMPLETED

--- app/src/main/java/com/example/uddishverma22/mait_go/Activities/Announcements.java
+++ app/src/main/java/com/example/uddishverma22/mait_go/Activities/Announcements.java
@@ -2,14 +2,85 @@ package com.example.uddishverma22.mait_go.Activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.example.uddishverma22.mait_go.Adapters.AnnouncementAdapter;
+import com.example.uddishverma22.mait_go.Adapters.NoticeAdapter;
+import com.example.uddishverma22.mait_go.Models.ClassAnnouncementsModel;
 import com.example.uddishverma22.mait_go.R;
+import com.example.uddishverma22.mait_go.Utils.VolleySingleton;
+import com.wang.avi.AVLoadingIndicatorView;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
 
 public class Announcements extends AppCompatActivity {
+
+    public static final String TAG = "Announcements";
+
+    String url = "https://agile-hamlet-82527.herokuapp.com/announcement/4I4";
+    JSONObject object;
+    ClassAnnouncementsModel announcement;
+    public ArrayList<ClassAnnouncementsModel> list = new ArrayList<>();
+
+    public RecyclerView recyclerView;
+    public AnnouncementAdapter mAdapter;
+
+    AVLoadingIndicatorView avi;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_announcements);
+
+        RequestQueue queue = VolleySingleton.getInstance(this).getRequestQueue();
+
+        recyclerView = (RecyclerView) findViewById(R.id.recycler_view);
+
+        avi = (AVLoadingIndicatorView) findViewById(R.id.avi);
+        avi.show();
+
+        JsonArrayRequest request = new JsonArrayRequest(Request.Method.GET, url, null,
+                new Response.Listener<JSONArray>() {
+                    @Override
+                    public void onResponse(JSONArray response) {
+                        avi.hide();
+                        Log.d(TAG, "onResponse: " + response);
+                        try {
+                            for (int i = 0; i < response.length(); i++) {
+
+                                object = response.getJSONObject(i);
+                                announcement = new ClassAnnouncementsModel();
+                                announcement.teacherName = object.getString("name");
+                                announcement.announcement = object.getString("message");
+                                list.add(announcement);
+                            }
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+
+                        mAdapter = new AnnouncementAdapter(list);
+                        RecyclerView.LayoutManager mLayoutManager = new LinearLayoutManager(Announcements.this);
+                        recyclerView.setLayoutManager(mLayoutManager);
+                        recyclerView.setAdapter(mAdapter);
+
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+
+            }
+        });
+        queue.add(request);
     }
 }
