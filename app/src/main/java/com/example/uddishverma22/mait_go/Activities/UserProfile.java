bac655af5d7ff107a9373b0a0e9fc9bc9865c99d
uddish <uddishverma22@gmail.com>
2017-05-06 10:58:29 +0530

    Image set in profile page

--- app/src/main/java/com/example/uddishverma22/mait_go/Activities/UserProfile.java
+++ app/src/main/java/com/example/uddishverma22/mait_go/Activities/UserProfile.java
@@ -1,16 +1,22 @@
 package com.example.uddishverma22.mait_go.Activities;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.content.res.Resources;
 import android.graphics.Bitmap;
+import android.graphics.Point;
 import android.graphics.Typeface;
-import android.os.AsyncTask;
+import android.graphics.drawable.Drawable;
+import android.graphics.drawable.LayerDrawable;
+import android.net.Uri;
+import android.os.Build;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Display;
 import android.view.LayoutInflater;
 import android.view.View;
-import android.view.animation.Animation;
-import android.view.animation.AnimationUtils;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
@@ -19,30 +25,42 @@ import android.widget.ScrollView;
 import android.widget.TextView;
 
 import com.example.uddishverma22.mait_go.R;
-import com.example.uddishverma22.mait_go.Utils.ImageLoader;
 import com.google.zxing.BarcodeFormat;
 import com.google.zxing.EncodeHintType;
 import com.google.zxing.MultiFormatWriter;
 import com.google.zxing.WriterException;
 import com.google.zxing.common.BitMatrix;
+import com.squareup.picasso.Picasso;
 
-import java.io.ByteArrayOutputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
 import java.util.EnumMap;
 import java.util.Map;
-import java.util.concurrent.ExecutionException;
+
+import de.hdodenhof.circleimageview.CircleImageView;
 
 
-public class UserProfile extends AppCompatActivity{
+public class UserProfile extends AppCompatActivity {
 
     public static final String TAG = "UserProfile";
 
 
-    TextView name, roll, branch, semester,className, branchHeading, classHeading, semesterHeading;
+    TextView name, roll, branch, semester, className, branchHeading, classHeading, semesterHeading;
     ImageView orangeThemeButton;
     public static int themeColor = 0;
     RelativeLayout diagonalLayout;
     LinearLayout branchSelector, semesterSelector, classSelector, outlineBox;
     Typeface tf;
+
+    //Components to change image in the profile page
+    CircleImageView profileImage;
+    public static final int IMAGE_CODE = 2990;
+    public static final int KITKAT_VALUE = 1002;
+    private static Uri profilePicUri;
+    Drawable profilePicDrawable;
+    ImageView dpImage, blurredBackImage;
+    Display display;
+    Point size;
 
     //barcode ticket components
     ImageView leftCircle, rightCircle, barcodeImg;
@@ -58,7 +76,9 @@ public class UserProfile extends AppCompatActivity{
     View alertLayout;
     TextView alertCs, alertIt, alertEce, alertEee, alertMae;            //tv for the popup branch menu
     TextView first, second, third, fourth, fifth, sixth, seventh, eighth;            //tv for the popup semester menu
-    EditText classEditText; TextView classAlertHeading; ImageView classImg;                            //tv and et for the popup class menu
+    EditText classEditText;
+    TextView classAlertHeading;
+    ImageView classImg;                            //tv and et for the popup class menu
 
 
     /**
@@ -70,10 +90,15 @@ public class UserProfile extends AppCompatActivity{
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_profile);
 
-        tf = Typeface.createFromAsset(getApplicationContext().getAssets(),"fonts/Raleway-Regular.ttf");
+        tf = Typeface.createFromAsset(getApplicationContext().getAssets(), "fonts/Raleway-Regular.ttf");
 
         name = (TextView) findViewById(R.id.name_tv);
         roll = (TextView) findViewById(R.id.enrollment_tv);
+
+        profileImage = (CircleImageView) findViewById(R.id.circleImageView);
+        blurredBackImage = (ImageView) findViewById(R.id.profile_blur_img);
+        display = getWindowManager().getDefaultDisplay();
+        size = new Point();
 
 
         branch = (TextView) findViewById(R.id.branch_tv);
@@ -88,6 +113,20 @@ public class UserProfile extends AppCompatActivity{
         semesterSelector = (LinearLayout) findViewById(R.id.semester_selector);
         classSelector = (LinearLayout) findViewById(R.id.class_selector);
 
+        //TODO Store the profile pic in shared preference
+        //Setting the profile pic
+        if (profilePicUri != null) {
+            Picasso.with(this).load(profilePicUri).into(profileImage);
+            Picasso.with(this).load(profilePicUri).into(blurredBackImage);
+
+            //Getting screen's width and height
+            display.getSize(size);
+            int width = size.x;
+            int height = size.y;
+
+            Picasso.with(this).load(profilePicUri).resize(width, 1000).centerCrop().into(blurredBackImage);
+
+        }
 
         //Theme selectors
         orangeThemeButton = (ImageView) findViewById(R.id.orange_theme);
@@ -129,6 +168,25 @@ public class UserProfile extends AppCompatActivity{
         //Custom alert dialog to pick the branch
         alert = new AlertDialog.Builder(UserProfile.this);
 
+        profileImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent;
+
+                if (Build.VERSION.SDK_INT < 19) {
+                    intent = new Intent();
+                    intent.setAction(Intent.ACTION_GET_CONTENT);
+                    intent.setType("*/*");
+                    startActivityForResult(intent, KITKAT_VALUE);
+                } else {
+                    intent = new Intent(Intent.ACTION_OPEN_DOCUMENT);
+                    intent.addCategory(Intent.CATEGORY_OPENABLE);
+                    intent.setType("*/*");
+                    startActivityForResult(intent, KITKAT_VALUE);
+                }
+
+            }
+        });
 
         branchSelector.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -151,11 +209,11 @@ public class UserProfile extends AppCompatActivity{
 
 
         //Checking for theme
-        if(themeColor == 101)   {
+        if (themeColor == 101) {
             diagonalLayout.setBackgroundResource(R.drawable.diagonal_background_yellow);
         }
 
-        tf = Typeface.createFromAsset(getApplicationContext().getAssets(),"fonts/Raleway-Regular.ttf");
+        tf = Typeface.createFromAsset(getApplicationContext().getAssets(), "fonts/Raleway-Regular.ttf");
         name.setTypeface(tf);
 
         orangeThemeButton.setOnClickListener(new View.OnClickListener() {
@@ -173,7 +231,59 @@ public class UserProfile extends AppCompatActivity{
         });
     }
 
-    private void alertBranchDialog()    {
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == KITKAT_VALUE) {
+            if (resultCode == Activity.RESULT_OK) {
+                profilePicUri = data.getData();
+                Picasso.with(this).load(profilePicUri).into(profileImage);
+                //Getting screen's width and height
+                display.getSize(size);
+                int width = size.x;
+                int height = size.y;
+
+                Picasso.with(this).load(profilePicUri).resize(width, 1000).centerCrop().into(blurredBackImage);
+
+//                Drawable img = getDrawableFromUri();
+//                Resources res = getApplicationContext().getResources();
+//                LayerDrawable mDrawable = (LayerDrawable) res.getDrawable(R.drawable.diagonal_background_blue);
+//                mDrawable.setDrawableByLayerId(R.id.diagonal_profile_image, img);
+//                diagonalLayout.setBackground(mDrawable);
+                //Bitmap layer2 = MediaStore.Images.Media.getBitmap(this.getContentResolver(), profilePicUri);
+//                Drawable layer1 = getResources().getDrawable( R.drawable.layer_one);
+//                Drawable layer2 = getResources().getDrawable( R.drawable.profile);
+//                Drawable layer3 = getResources().getDrawable( R.drawable.back_diagonal);
+//                Drawable[] layers = {layer1, layer2, layer3};
+//                LayerDrawable splash_test = new LayerDrawable(layers);
+//                diagonalLayout.setBackground(splash_test);
+
+            }
+        }
+        if (requestCode == IMAGE_CODE && resultCode == RESULT_OK && data != null && data.getData() != null) {
+            profilePicUri = data.getData();
+            Picasso.with(this).load(profilePicUri).into(profileImage);
+            //Getting screen's width and height
+            display.getSize(size);
+            int width = size.x;
+            int height = size.y;
+            //Loading the blurred image
+            Picasso.with(this).load(profilePicUri).resize(width, 1000).centerCrop().into(blurredBackImage);
+        }
+    }
+
+    private Drawable getDrawableFromUri() {
+        try {
+            InputStream stream = getContentResolver().openInputStream(profilePicUri);
+            profilePicDrawable = Drawable.createFromStream(stream, profilePicUri.toString());
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        return profilePicDrawable;
+    }
+
+    private void alertBranchDialog() {
 
         LayoutInflater inflater = getLayoutInflater();
         alertLayout = inflater.inflate(R.layout.branch_selector_layout, null);
@@ -226,7 +336,7 @@ public class UserProfile extends AppCompatActivity{
 
     }
 
-    private void alertSemesterDialog()    {
+    private void alertSemesterDialog() {
 
         LayoutInflater inflater = getLayoutInflater();
         alertLayout = inflater.inflate(R.layout.semester_selector_layout, null);
@@ -327,7 +437,7 @@ public class UserProfile extends AppCompatActivity{
 
     Bitmap encodeAsBitmap(String contents, BarcodeFormat format, int img_width, int img_height) {
         String contentsToEncode = contents;
-        if(contentsToEncode == null)    {
+        if (contentsToEncode == null) {
             return null;
         }
 
