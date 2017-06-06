c2b2268180ff4a73a54093edad11cc010ffc63b9
uddish <uddishverma22@gmail.com>
2017-06-07 01:17:31 +0530

    Result api changed

--- app/src/main/java/com/example/uddishverma22/mait_go/Activities/Result.java
+++ app/src/main/java/com/example/uddishverma22/mait_go/Activities/Result.java
@@ -1,22 +1,16 @@
 package com.example.uddishverma22.mait_go.Activities;
 
-import android.graphics.drawable.Drawable;
 import android.support.design.widget.CollapsingToolbarLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
-import android.webkit.WebView;
-import android.widget.ProgressBar;
-import android.widget.SeekBar;
 import android.widget.Toast;
-
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
-import com.android.volley.toolbox.JsonArrayRequest;
 import com.android.volley.toolbox.JsonObjectRequest;
 import com.example.uddishverma22.mait_go.Adapters.ResultAdapter;
 import com.example.uddishverma22.mait_go.Models.ResultModel;
@@ -38,7 +32,7 @@ import io.realm.RealmResults;
 public class Result extends AppCompatActivity {
 
     String roll = "40514803115";
-    String url = "https://agile-hamlet-82527.herokuapp.com/scrape/result/" + roll;
+    String url = "http://ec2-52-66-87-230.ap-south-1.compute.amazonaws.com/result/" + roll;
     JSONObject jsonObject;
     public static String percentage = null;
 
@@ -85,7 +79,7 @@ public class Result extends AppCompatActivity {
                         try {
                             avi.hide();
                             JSONArray jsonArray = response.getJSONArray("marks");
-                            percentage = (String) response.get("percentage");
+                            percentage = String.valueOf((int) response.get("percentage"));
                             //Setting the percentage in the circleView
                             mCircleView.setValueAnimated(Float.parseFloat(percentage));
                             for (int i = 0; i < jsonArray.length(); i++) {
@@ -97,7 +91,7 @@ public class Result extends AppCompatActivity {
                                 resultObj.extMarks = jsonObject.getString("external");
                                 resultObj.credits = jsonObject.getString("credits");
                                 resultObj.totMarks = jsonObject.getString("total");
-                                resultObj.percentage = (String) response.get("percentage");
+                                resultObj.percentage = String.valueOf((int) response.get("percentage"));
                                 resultList.add(resultObj);
                             }
 
